cc13fcca82435bb85d8f2dca0dc51355d9f83087
valentin.dubrovskiy <v.dubrovskiy@rambler-co.ru>
2017-09-17 01:49:18 +0300

    first commit

---
+++ example/simple.js
@@ -0,0 +1,77 @@
+const {
+    start,
+    json,
+    match,
+    getUrl,
+    getQuery,
+    getBody
+} = require('../lib');
+
+let heroes = [
+    { id: 1, superhero: 'Superman', rating: 10 },
+    { id: 2, superhero: 'Batman', rating: 20 },
+    { id: 3, superhero: 'Spider-Man', rating: 5 }
+];
+
+const getHeroes = () => Promise.resolve(heroes); // for async
+
+function* mainHandler() {
+    yield match('GET', '/');
+
+    const url = yield getUrl();
+    const { city } = yield getQuery();
+
+    return json({
+        title: 'This site is about superheroes.',
+        bestSuperhero: city === 'metropolis' ? 'Superman' : 'Batman',
+        currentUrl: url
+    });
+}
+
+function* heroesHandler() {
+    yield match('GET', '/heroes');
+
+    const { sortByRating } = yield getQuery();
+
+    let heroes = yield getHeroes();
+
+    if (sortByRating) {
+        heroes = heroes.sort((prev, current) => {
+            if (current.rating > prev.rating) {
+                return 1;
+            } else if (current.rating < prev.rating) {
+                return -1;
+            } else {
+                return 1;
+            }
+        });
+    }
+
+    return json({ heroes });
+}
+
+function* heroHandler() {
+    const { id } = yield match('GET', '/heroes/:id');
+
+    return getHeroes()
+        .then(heroes => heroes.find(hero => hero.id === Number(id)))
+        .then(hero => json({ hero }));
+}
+
+function* addSuperhero() {
+    yield match('POST', '/heroes');
+
+    const { name, rating } = yield getBody('json');
+    const hero = { id: Math.round(Date.now() / 1000), name, rating };
+
+    heroes.push(hero);
+
+    return json({ hero });
+}
+
+start(
+    mainHandler,
+    heroesHandler,
+    heroHandler,
+    addSuperhero
+)(3000);
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Blaker
+
+A modern and usable web framework for Node.js.
+
+```js
+const { start, getUrl, getQuery, match, json } = require('../lib');
+
+function* mainHandler() {
+    const { id } = yield match('GET', '/users/:id');
+
+    const url = yield getUrl(); // get current url
+    const query = yield getQuery(); // get all query
+
+    return json({ paramId: id, currentUrl: url, requestQuery: query });
+}
+
+start(mainHandler)(3000);
+```
